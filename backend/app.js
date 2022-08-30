ec3da43b77305ee2a963b909f5648ed59105ac6b
NeihTzxc <45267625+NeihTzxc@users.noreply.github.com>
2022-08-30 09:36:45 +0700

    order api

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,15 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const product = require('./routes/product.route');
 const user = require('./routes/user.router')
-const errorMiddleware = require('./middleware/error')
+const order = require('./routes/order.route')
+const errorMiddleware = require('./middleware/error');
 app.use(express.json());
 app.use(cookieParser())
 
 //Route Imports
 app.use("/api/v1", product);
 app.use("/api/v1", user)
-
+app.use("/api/v1", order)
 app.use(errorMiddleware)
 
 module.exports = app
--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,6 +1,6 @@
 const ErrorHandle = require('../utils/errorhandle');
 module.exports = (err, req, res, next) => {
-    console.error(err)
+    console.error(err);
     err.statusCode = err.statusCode | 500;
     err.message = err.message || "Internal Server Error";
     //Wrong Mongodb Id error
@@ -13,7 +13,7 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandle(message, 400);
     }
     //Wrong JWT error
-    if (err.name = 'JsonWebTokenError') {
+    if (err.name === 'JsonWebTokenError') {
         const message = `Json Web Token is invalid, try again`;
         err = new ErrorHandle(message, 400);
     }
